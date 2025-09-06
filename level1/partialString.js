425cf9199d22d36a901e6c0479e8d2108e450c9e
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-09-06 16:22:12 +0900

    feat: 크기가 작은 부분 문자열 문제 풀이 완료

--- level1/partialString.js
+++ level1/partialString.js
@@ -1,16 +1,13 @@
 function solution(t, p) {
   const arr = t.split("");
   const result = [];
-  for (let i = 0; i < arr.length; i++) {
-    let num = i;
-    for (let j = 0; j < p.length - 1; j++) {
-      if (!t[j + 1]) continue;
-      num = num + t[j + 1];
-    }
-    result.push(+num);
+  for (idx in t) {
+    let num = arr.slice(+idx, +idx + p.length);
+    num = num.join("");
+    if (num.length === p.length) result.push(+num);
   }
   return result.filter((r) => r <= +p).length;
 }
-solution("3141592", "271");
+solution("10203", "15");
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/147355
