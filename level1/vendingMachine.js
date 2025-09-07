d9415bda14c084ae100796c2e81a710044bc0b55
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-09-07 16:29:16 +0900

    feat: 문자가 한개라도 포함 안된경우는 무조건 -1리턴하도록 수정

--- level1/vendingMachine.js
+++ level1/vendingMachine.js
@@ -10,14 +10,17 @@ function solution(keymap, targets) {
   });
   return targets.map((target) => {
     let count = 0;
-    target.split("").forEach((t) => {
+    for (const t of target) {
       if (minPress[t]) {
         count += minPress[t];
       } else {
-        count = -1;
+        count = -1; // 한 번이라도 없으면 전체 -1
+        break; // 더 이상 계산할 필요 없음
       }
-    });
+    }
     return count;
   });
 }
 console.log(solution(["ABACD", "BCEFD"], ["ABCD", "AABB"]));
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/160586
