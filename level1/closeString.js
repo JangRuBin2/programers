e421f17e01b644d23600c8ba36e0e05358f496c3
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-09-07 16:49:52 +0900

    chore: split보기 싫어서 전개 연산자로 수정

--- level1/closeString.js
+++ level1/closeString.js
@@ -1,6 +1,6 @@
 function solution(s) {
-  return s.split("").map((string, idx) => {
-    const front = s.slice(0, idx).split("");
+  return [...s].map((string, idx) => {
+    const front = [...s.slice(0, idx)];
     // findIndex는 가장 처음 나오는 index를 반환하기 때문에 가장 가까운 인덱스를 구하기 위해서 배열을 reverse했습니다.
     const targetIndex = front.reverse().findIndex((f) => f === string);
     return targetIndex === -1 ? -1 : targetIndex + 1;
